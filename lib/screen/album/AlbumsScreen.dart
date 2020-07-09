ea8ccdd8bef061990f7046dd9fabfc8ae2f4c508
Adrian R <adrian09h@gmail.com>
2020-07-09 20:03:18 +0200

    Add Photos screen

--- lib/screen/album/AlbumsScreen.dart
+++ lib/screen/album/AlbumsScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutteralbum/screen/album/PhotosScreen.dart';
 import 'package:flutteralbum/view/AlbumCell.dart';
+import 'package:page_transition/page_transition.dart';
 
 class AlbumsScreen extends StatefulWidget {
   @override
@@ -20,7 +22,16 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
           padding: EdgeInsets.all(12),
           crossAxisCount: 2,
           childAspectRatio: 2,
-          children: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""].map((e) => AlbumCell()).toList()
+          children: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""].map((e) => AlbumCell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.rightToLeft,
+                      child: PhotosScreen(
+                      )));
+            },
+          )).toList()
       ),
     );
   }
--- lib/view/PhotoCell.dart
+++ lib/view/PhotoCell.dart
@@ -1,8 +1,64 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PhotoCell extends StatelessWidget {
+  final VoidCallback onTap;
+  const PhotoCell({this.onTap});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+      child: Container(
+        padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: Colors.black12),
+        child: GestureDetector(
+          onTap: onTap,
+          child: Row(
+            children: [
+              Container(
+                width: 120,
+                height: 120,
+                child: CachedNetworkImage(
+                  imageUrl: "https://via.placeholder.com/150/d32776",
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              ),
+              SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Album id: 123",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "id: 123",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
